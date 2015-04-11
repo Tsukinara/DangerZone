3eecadf8150f38da5eaa63514be3aecddabe2532
Patrick Owen <patowen95@gmail.com>
2015-04-11 15:22:04 -0400

    Switch from Maryland Coordinates to Lat/Long
    Approximately

--- DangerZone/DangerZone/app/src/main/java/dangerzone/dangerzone/XmlParser.java
+++ DangerZone/DangerZone/app/src/main/java/dangerzone/dangerzone/XmlParser.java
@@ -66,6 +66,33 @@ public class XmlParser {
         public String blocksiteaddress;
         public double blockxcoord;
         public double blockycoord;
+        public double latitude;
+        public double longitude;
+
+        public Content() {
+            blockxcoord = 350000; blockycoord=160000;
+            convertCoordinates();
+        }
+
+        //387000,125000 -> 38.792656145954,-77.1496413967816
+        //405000,125000 -> 38.7927379317937,-76.9424455726982
+        //387000,146000 -> 38.9818317605639,-77.1500386930623
+
+        //18000,0 -> 8.178583970419595e-5, 0.2071958240833993
+        //0,21000 -> 0.1891756146099013, -3.972962806955138e-4
+
+        //1,0 -> 4.54365776134422e-9, 1.151087911574441e-5
+        //0,1 -> 9.00836260047149e-6, -1.891887050931018e-8
+
+        public void convertCoordinates() {
+            double translatedxcoord = blockxcoord - 387000;
+            double translatedycoord = blockycoord - 125000;
+            double dlat = 4.54365776134422e-9*translatedxcoord + 9.00836260047149e-6*translatedycoord;
+            double dlong = 1.151087911574441e-5*translatedxcoord - 1.891887050931018e-8*translatedycoord;
+
+            latitude = dlat + 38.792656145954;
+            longitude = dlong - 77.1496413967816;
+        }
 
         public String toString() {
             StringBuilder builder = new StringBuilder();
@@ -73,8 +100,8 @@ public class XmlParser {
             builder.append(reportdatetime + ", ");
             builder.append(offense + ", ");
             builder.append(blocksiteaddress + ", ");
-            builder.append(blockxcoord + ", ");
-            builder.append(blockycoord + "}");
+            builder.append(latitude + ", ");
+            builder.append(longitude + "}");
 
             return builder.toString();
         }
@@ -144,6 +171,7 @@ public class XmlParser {
             }
         }
 
+        output.convertCoordinates();
         return output;
     }
 
@@ -151,7 +179,6 @@ public class XmlParser {
         parser.require(XmlPullParser.START_TAG, ns, "dcst:reportdatetime");
         String str = removeCData(readText(parser));
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ", Locale.US);
-        System.out.println(format.format(new Date()));
         Date date = format.parse(str);
         parser.require(XmlPullParser.END_TAG, ns, "dcst:reportdatetime");
         return date;
@@ -213,9 +240,6 @@ public class XmlParser {
     }
 
     private String removeCData(String str) {
-        /*System.out.println("["+str+"]");
-        return str.substring(10, str.length()-4);*/
-        System.out.println(str);
         return str;
     }
 }

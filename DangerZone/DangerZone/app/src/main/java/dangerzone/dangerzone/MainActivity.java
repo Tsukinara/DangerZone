a0082072f1e28accc10819c418d93668106ba4ae
Patrick Owen <patowen95@gmail.com>
2015-04-11 06:23:48 -0400

    Add DangerZone to Git

---
+++ DangerZone/DangerZone/app/src/main/java/dangerzone/dangerzone/MainActivity.java
@@ -0,0 +1,120 @@
+package dangerzone.dangerzone;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.source);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute("http://data.octo.dc.gov/feeds/crime_incidents/crime_incidents_current.xml");
+        } else {
+            textView.setText("Error");
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500000;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("HttpExample", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            return readIt(is, len);
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException {
+        XmlParser parser = new XmlParser();
+        try {
+            return parser.parse(stream).toString();
+        } catch (XmlPullParserException e) {
+            return "Oh no! We are doomed!";
+        }
+    }
+}
---
+++ DangerZone/DangerZone/app/src/main/java/dangerzone/dangerzone/XmlParser.java
@@ -0,0 +1,215 @@
+package dangerzone.dangerzone;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Patrick on 4/11/2015
+ */
+public class XmlParser {
+    private static final String ns = null;
+
+    public List<Entry> parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List<Entry> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<Entry> entries = new ArrayList<>();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+
+            if (name.equals("entry")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+
+        return entries;
+    }
+
+    public static class Entry {
+        public Content content;
+
+        public String toString() {
+            return content.toString();
+        }
+    }
+
+    public static class Content {
+        public String reportdatetime;
+        public String offense;
+        public String blocksiteaddress;
+        public double blockxcoord;
+        public double blockycoord;
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append("{");
+            builder.append(reportdatetime + ", ");
+            builder.append(offense + ", ");
+            builder.append(blocksiteaddress + ", ");
+            builder.append(blockxcoord + ", ");
+            builder.append(blockycoord + "}");
+
+            return builder.toString();
+        }
+    }
+
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Entry output = new Entry();
+
+        parser.require(XmlPullParser.START_TAG, ns, "entry");
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("content")) {
+                output.content = readContent(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return output;
+    }
+
+    private Content readContent(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Content output = null;
+
+        parser.require(XmlPullParser.START_TAG, ns, "content");
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("dcst:ReportedCrime")) {
+                output = readDcst(parser);
+            } else {
+                skip(parser);
+            }
+        }
+
+        return output;
+    }
+
+    private Content readDcst(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Content output = new Content();
+
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:ReportedCrime");
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("dcst:reportdatetime")) {
+                output.reportdatetime = readDateTime(parser);
+            } else if (name.equals("dcst:offense")) {
+                output.offense = readOffense(parser);
+            } else if (name.equals("dcst:blocksiteaddress")) {
+                output.blocksiteaddress = readSiteAddress(parser);
+            } else if (name.equals("dcst:blockxcoord")) {
+                output.blockxcoord = readXCoord(parser);
+            } else if (name.equals("dcst:blockycoord")) {
+                output.blockycoord = readYCoord(parser);
+            } else {
+                skip(parser);
+            }
+        }
+
+        return output;
+    }
+
+    private String readDateTime(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:reportdatetime");
+        String str = removeCData(readText(parser));
+        //TODO regexp
+        parser.require(XmlPullParser.END_TAG, ns, "dcst:reportdatetime");
+        return str;
+    }
+
+    private String readOffense(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:offense");
+        String str = removeCData(readText(parser));
+        parser.require(XmlPullParser.END_TAG, ns, "dcst:offense");
+        return str;
+    }
+
+    private String readSiteAddress(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:blocksiteaddress");
+        String str = removeCData(readText(parser));
+        parser.require(XmlPullParser.END_TAG, ns, "dcst:blocksiteaddress");
+        return str;
+    }
+
+    private double readXCoord(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:blockxcoord");
+        String str = removeCData(readText(parser));
+        parser.require(XmlPullParser.END_TAG, ns, "dcst:blockxcoord");
+        return Double.parseDouble(str);
+    }
+
+    private double readYCoord(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "dcst:blockycoord");
+        String str = removeCData(readText(parser));
+        parser.require(XmlPullParser.END_TAG, ns, "dcst:blockycoord");
+        return Double.parseDouble(str);
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+    private String removeCData(String str) {
+        /*System.out.println("["+str+"]");
+        return str.substring(10, str.length()-4);*/
+        System.out.println(str);
+        return str;
+    }
+}
